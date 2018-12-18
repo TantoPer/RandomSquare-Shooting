e85e95b687f86c4e99fc74008a5186b551ba9409
DanerSound <andre.barreto28@gmail.com>
2018-12-18 23:31:46 +0100

    added square class, some dummy changes in frame

--- RandomSquare-Shooting/src/tantoPer/RSshooting/Frame.java
+++ RandomSquare-Shooting/src/tantoPer/RSshooting/Frame.java
@@ -8,19 +8,29 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class Frame {
-	
+
 	private JLabel counter;
 	private JLabel imgToView;
 	private JFrame frame;
+	private Square squares[];
 	private int position = 0;
-	
+
 	Random rand = new Random();
 
 	public Frame(MTC mtc) {
-		frame = new JFrame("Move the Cicle");
-		
+		frame = new JFrame("Start");
+		squares = new Square[2];
+
+		for (int i = 0; i < squares.length; i++) {
+			squares[i] = squareFactory(randomPosition(i), randomPosition(i), 50, 50, "circle.png");
+			
+			Icon image = new ImageIcon(getClass().getResource(squares[i].getFileName()));
+			imgToView = new JLabel(image);
+			imgToView.setBounds(squares[i].getxPosition(), squares[i].getSizeY(), 50, 50);
+			frame.add(imgToView).setVisible(true);
+		}
+
 		frame.setSize(500, 400);
 		Container myContainer = frame.getContentPane();
 		myContainer.setLayout(null);
@@ -31,6 +41,47 @@ public class Frame {
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+
+	private int randomPosition(int i) {
+		return 150 + 100 * i;
+	}
+
+	private Square squareFactory(int xPosition, int yPosition, int sizeX, int sizeY, String fileName) {
+		return new Square(xPosition, yPosition, sizeX, sizeY, fileName);
+	}
+
+	private void imagineLoader(String fileName) {
+		Icon image = new ImageIcon(getClass().getResource(fileName));
+		imgToView = new JLabel(image);
+		imgToView.setBounds(150, 150, 50, 50);
+		frame.add(imgToView).setVisible(true);
+
+	}
+
+	public void imagineNewPosition() {
+		int randomX, randomY;
+		randomX = rand.nextInt(500);
+		randomY = rand.nextInt(400);
+		if (getPosition() == 0) {
+			imgToView.setVisible(false);
+			imgToView.setBounds(randomX, randomY, 50, 50);
+			frame.add(imgToView).setVisible(true);
+			setPosition(1);
+		} else {
+			imgToView.setVisible(false);
+			imgToView.setBounds(randomX, randomY, 50, 50);
+			frame.add(imgToView).setVisible(true);
+			setPosition(0);
+		}
+	}
+
+	private void createPointString() {
+		JLabel pointString = new JLabel();
+		pointString.setText("Points:");
+		pointString.setBounds(100, 300, 100, 50);
+		frame.add(pointString);
+
 	}
 
 	private void createButton(JFrame frame, MTC mtc) {
@@ -47,39 +98,6 @@ public class Frame {
 		frame.add(counter);
 	}
 
-	private void imagineLoader(String fileName) {
-		Icon image = new ImageIcon(getClass().getResource(fileName));
-		imgToView = new JLabel(image);
-		imgToView.setBounds(150, 150, 50, 50);
-		frame.add(imgToView).setVisible(true);
-
-	}
-
-	public void imagineNewPosition() {
-		int randomX,randomY;
-		randomX=rand.nextInt(500);
-		randomY=rand.nextInt(400);
-		if(getPosition()==0) {
-			imgToView.setVisible(false);	
-			imgToView.setBounds(randomX, randomY, 50, 50);
-			frame.add(imgToView).setVisible(true);
-			setPosition(1);
-		}else {
-			imgToView.setVisible(false);
-			imgToView.setBounds(randomX, randomY, 50, 50);
-			frame.add(imgToView).setVisible(true);
-			setPosition(0);	
-		}
-	}
-	
-	private void createPointString() {
-		JLabel pointString = new JLabel();
-		pointString.setText("Points:");
-		pointString.setBounds(100, 300, 100, 50);
-		frame.add(pointString);
-
-	}
-
 	public void updateCounter(String text) {
 		this.counter.setText(text);
 	}
@@ -91,7 +109,5 @@ public class Frame {
 	public void setPosition(int position) {
 		this.position = position;
 	}
-
-	
 
 }
